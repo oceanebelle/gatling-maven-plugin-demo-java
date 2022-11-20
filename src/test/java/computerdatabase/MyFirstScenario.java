036118a339de566795ecc1247c535633a8e2b572
oceanebelle <oceanebelle@users.noreply.github.com>
2022-11-20 22:42:49 +0000

    cd /home/oceanebelle/dev/gatling-maven-plugin-demo-java ; /usr/bin/env /usr/lib/jvm/java-16-openjdk-amd64/bin/java -XX:+ShowCodeDetailsInExceptionMessages @/tmp/cp_zs9ofq7nkvp29vnmc89kr6nn.argfile Engine

--- src/test/java/computerdatabase/MyFirstScenario.java
+++ src/test/java/computerdatabase/MyFirstScenario.java
@@ -19,15 +19,14 @@ public class MyFirstScenario extends Simulation {
             .check(
                 status().is(200),
                 jsonPath("$[*].id").findRandom().saveAs("gameid"),
-                jsonPath("$[*].id").findAll().saveAs("gameidlist"))
-    ).pause(1);            
+                jsonPath("$[*].id").findAll().saveAs("gameidlist")));            
     
-    ChainBuilder fetchVideo = exec(MyFirstScenario::logGameId).exec(
-        http("Get Video").get("/videogame/#{gameid}")
+    ChainBuilder fetchVideo = exec(LogHelper::logGameId).exec(
+        http("Get Video id=#{gameid}").get("/videogame/#{gameid}")
             .check(status().is(200)));
 
-    ChainBuilder fetchEveryVideo = exec(MyFirstScenario::logSessionVariable)
-        .foreach("#{gameidlist}", "gameid").on(fetchVideo);
+    ChainBuilder fetchEveryVideo = exec(
+        foreach("#{gameidlist}", "gameid").on(fetchVideo));
     
     ScenarioBuilder visitOne = scenario("Visit one")
         .exec(fetchAllVideos, fetchVideo);
@@ -37,24 +36,23 @@ public class MyFirstScenario extends Simulation {
     
     {
         setUp(
-            // define populations and work models
             visitOne.injectOpen(rampUsers(10).during(10)),
-            visitAll.injectOpen(rampUsers(1).during(10))
+            visitAll.injectOpen(nothingFor(6), atOnceUsers(1))
         ).protocols(httpProtocol)
         .assertions(
             global().responseTime().max().lt(150),
             global().allRequests().percent().gte(99.0));
     }
 
-
-    static Session logSessionVariable(Session session) {
-        System.out.println(session.getString("gameidlist"));
-        return (session);
+    static class LogHelper {
+        static Session logGameIdList(Session session) {
+            System.out.println(session.getString("gameidlist"));
+            return (session);
+        }
+    
+        static Session logGameId(Session session) {
+            System.out.println(session.getString("gameid"));
+            return (session);
+        }
     }
-
-    static Session logGameId(Session session) {
-        System.out.println(session.getString("gameid"));
-        return (session);
-    }
-
 }
