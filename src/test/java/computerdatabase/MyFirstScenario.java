142b1186b344f2409ee33acfcedbdfc904a72107
oceanebelle <oceanebelle@users.noreply.github.com>
2022-11-20 21:51:17 +0000

    modified

--- src/test/java/computerdatabase/MyFirstScenario.java
+++ src/test/java/computerdatabase/MyFirstScenario.java
@@ -3,6 +3,8 @@ package computerdatabase;
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
+import com.redis.S;
+
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
 
@@ -11,30 +13,48 @@ public class MyFirstScenario extends Simulation {
     // Define the HTTP configuration and base URL
     HttpProtocolBuilder httpProtocol = http.baseUrl("https://videogamedb.uk/api")
             .acceptHeader("application/json");
-
-    // A call chain that fetches a random id and save to session
+    
     ChainBuilder fetchAllVideos = exec(
         http("Get All videos").get("/videogame")
-            .check(status().is(200))            
-            .check(jsonPath("$[*].id").findRandom().saveAs("gameid"))
-        ).pause(1);
-            
-    // A call chain that fetches given an id from the user session and checks the http response
-    ChainBuilder fetchVideo = exec(
+            .check(
+                status().is(200),
+                jsonPath("$[*].id").findRandom().saveAs("gameid"),
+                jsonPath("$[*].id").findAll().saveAs("gameidlist"))
+    ).pause(1);            
+    
+    ChainBuilder fetchVideo = exec(MyFirstScenario::logGameId).exec(
         http("Get Video").get("/videogame/#{gameid}")
-            .check(responseTimeInMillis().lte(50))
             .check(status().is(200)));
 
-    // A Scenario that executes two chains sequentially
-    ScenarioBuilder scenario = scenario("Drill down Videos")
+    ChainBuilder fetchEveryVideo = exec(MyFirstScenario::logSessionVariable)
+        .foreach("#{gameidlist}", "gameid").on(fetchVideo);
+    
+    ScenarioBuilder visitOne = scenario("Visit one")
         .exec(fetchAllVideos, fetchVideo);
 
-    // Define the simulation definition including user load.
-    // Attach the httpProtol to all scenarios in this simulation
+    ScenarioBuilder visitAll = scenario("Visit all")
+        .exec(fetchAllVideos, fetchEveryVideo);
+    
     {
         setUp(
-            scenario.injectOpen(rampUsers(10).during(10))
-        ).protocols(httpProtocol);
+            // define populations and work models
+            visitOne.injectOpen(rampUsers(10).during(10)),
+            visitAll.injectOpen(rampUsers(1).during(10))
+        ).protocols(httpProtocol)
+        .assertions(
+            global().responseTime().max().lt(150),
+            global().allRequests().percent().gte(99.0));
+    }
+
+
+    static Session logSessionVariable(Session session) {
+        System.out.println(session.getString("gameidlist"));
+        return (session);
+    }
+
+    static Session logGameId(Session session) {
+        System.out.println(session.getString("gameid"));
+        return (session);
     }
 
 }
